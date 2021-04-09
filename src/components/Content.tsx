a84b7aa8f9e7fc89935ed5ee771b49c2988fe6a7
Lucas Hermont <lucas.hermont@hotmail.com>
2021-04-09 00:18:27 -0300

    Componentizando conteudo e barra lateral

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,23 @@
-export function Content() {
+import { MovieCard } from '../components/MovieCard';
+import '../styles/content.scss';
+
+export function Content({ selectedGenre,movies }) {
+
+  return (
+    <>
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+      <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+    </>
+
+
+  )
   // Complete aqui
 }
